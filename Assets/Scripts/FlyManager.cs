a7527e59f54a7776f2cb38478057777459ec8b02
mycoding5555 <mycoding5555@gmail.com>
2025-12-16 12:04:57 +0700

    first_commit

---
+++ Assets/Scripts/FlyManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FlyManager : MonoBehaviour
+{
+    [SerializeField] private float _velocity = 1.5f;
+    [SerializeField] private float _rotateSpeed = 10f;
+
+    Rigidbody2D rigidbody2D;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            rigidbody2D.linearVelocity = Vector2.up * _velocity;
+        }
+    }
+}
