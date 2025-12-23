409274c3e16c835566598021026d5430ec02231e
mycoding5555 <mycoding5555@gmail.com>
2025-12-23 22:43:02 +0700

    Done For add Panel Score

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _currentScore;
+    [SerializeField] private TextMeshProUGUI _higtScore;
+
+    private int score;
+
+    public static ScoreManager instance;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+      void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    void Start ()
+    {
+        _currentScore.text= score.ToString();
+        _higtScore.text= PlayerPrefs.GetInt("HighScore",0).ToString();
+        RefreshHightScore();
+    }
+
+  
+    // Update is called once per frame
+    void RefreshHightScore()
+    {
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            _higtScore.text = score.ToString();
+        }
+        
+    }
+
+    public void RefreshScore()
+    {
+        score++;
+        _currentScore.text = score.ToString();
+        RefreshHightScore();
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
